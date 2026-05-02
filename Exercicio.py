894e5a638112a7c6ecfa753d3e40a955be89a505
G-Ten <Omegatwin24@gmail.com>
2026-05-02 16:37:44 +0200

    Nada de especial só testando o git

---
+++ Exercicio.py
@@ -0,0 +1,9 @@
+#python program
+print("Saudações")
+ilustre = "Como está hoje?"
+print("{}".format(ilustre))
+print(f"De novo {ilustre}")
+if 2 :
+	print("O dia está bonito")
+else :
+	print("Concordo")
