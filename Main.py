2bcd3aca2a16d833fb09e50ed3f388d8b756d99f
G-Ten <Omegatwin24@gmail.com>
2026-07-03 21:08:21 +0200

    Novo ficheiro

--- Main.py
+++ Main.py
@@ -24,12 +24,24 @@ def Ex2():
 	Num.sort()
 	print(f"O maior deles é {Num[2]}")
 
+def Ex21():
+	Num = []
+	for i in range(3):
+		Num.append(int(input(f"Insira o {i+1}º número ")))
+	print(f"O maior número é {max(Num)}")
+
 def Ex3():
 	Number = [[]] * 3
 	for i in range(3):
 		Number[i] = int(input(f"Insira o {i+1}º número"))
 	Number.sort()
 	print(f"O menor número é: {Number[0]} \nO maior número é: {Number[2]}")
+
+def Ex31():
+	Num = []
+	for i in range(3):
+		Num.append(int(input(f"Insira o {i+1}º número ")))
+	print(f"O menor número é {min(Num)} e o maior número é {max(Num)}")
 
 def Ex4():
 	i = int(input("i = "))
@@ -64,19 +76,17 @@ def Ex7():
 			break
 		print("É para inserir salário e não dívida,insira um valor positivo")
 	print(f"Antes do reajuste o salário é {salario}")
+	salario_antes = salario
 	if salario <= 5000:
 		percentual = 0.2
-		salario += salario * percentual
 	elif salario < 12000:
 		percentual = 0.15
-		salario += salario * percentual
 	elif salario < 25000:
 		percentual = 0.1
-		salario += salario * percentual
 	else:
 		percentual  = 0.05
-		salario += salario * percentual
-	print(f"O percentual do aumento é {percentual*100}% \nO valor do aumento é {round((percentual*salario)/(1+percentual),5)}\nNovo salário após o reajuste é {round(salario,5)}")
+	salario += salario * percentual
+	print(f"O percentual do aumento é {percentual*100}% \nO valor do aumento é {round((salario - salario_antes),5)}\nNovo salário após o reajuste é {round(salario,5)}")
 
 def Ex8():
 	Resposta =[[]] * 5
@@ -88,7 +98,7 @@ def Ex8():
 	contador = 0
 	for i in range(5):
 		Resposta[i] = Resposta[i].lower()
-		if Resposta[i] == "sim" or Resposta[i] == "s" or Resposta[i] == "y" or Resposta[i] == "yes" or Resposta[i] == "si":
+		if Resposta[i] == "sim" or Resposta[i] == "s" or Resposta[i] == "y" or Resposta[i] == "yes" or Resposta[i] == "si" or Resposta[i] == "oui":
 			contador = contador + 1
 	if contador == 2:
 		print("Suspeito")
@@ -100,10 +110,22 @@ def Ex8():
 		print("Inocente")
 	
 def Palindromo():
-	string = input("Insira uma string ")
-	string.lower()
+	string = (input("Insira uma string ")).lower()
 	if string[0::] == string[::-1]:
 		print("É palindromo")
 	else:
 		print("Não é palindromo")
-Ex8()
+def Collatz():
+	while True:
+		a = int(input("Insira um inteiro positivo "))
+		while a != 1 :
+			if a % 2 == 0:
+				a = a/2
+			else:
+				a = 3*a + 1
+			print(a)
+		n = int(input("1 \nVai inserir outro número? \n1-Sim \n2-Não \nR: "))
+		if n == 2:
+			break
+	
+		
